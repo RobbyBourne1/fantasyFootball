8b4bc4775926115407eec799f90f7e522ef3e1d6
Robby Bourne <robbybourne1@gmail.com>
2017-10-03 12:54:54 -0400

    Player Info Page set up and routed

---
+++ Controllers/PlayerInfoController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using fantasyFootball.Models;
+
+namespace fantasyFootball.Controllers
+{
+    public class PlayerInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
