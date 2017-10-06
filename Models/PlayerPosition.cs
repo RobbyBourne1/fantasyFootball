8624fa6a46f9a7ddad8ba0aa8860d50dcaf88abe
Robby Bourne <robbybourne1@gmail.com>
2017-10-06 11:39:56 -0400

    Fixed the enums

--- Models/PlayerPosition.cs
+++ Models/PlayerPosition.cs
@@ -2,16 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-
 namespace fantasyFootball.Models
 {
+    [DefaultValue(NOPOSITION)]
     public enum PlayerPosition
     {
-        QB,
-        RB,
-        WR,
-        TE,
-        K,
-        DST
+        NOPOSITION = 0,
+        QUARTERBACK,
+        RUNNINGBACK,
+        WIDERECEIVER,
+        TIGHTEND,
+        KICKER,
+        DEFENSIVESPECIALTEAMS
     }
 }
