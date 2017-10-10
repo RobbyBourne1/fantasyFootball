c23052708a9cde744ed4acb6c9beff51c6c4e51e
Robby Bourne <robbybourne1@gmail.com>
2017-10-10 14:16:17 -0400

    Setting up appending names

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,15 +4,36 @@ let playerForm = document.querySelector('form')
 let input = document.querySelector('input')
 let search = input.value
 let url = "/proxy"
+let displayOfPlayers = document.querySelector('.playerInfo')
+
+const nameSection = document.createElement('div')
+const firstbreak = document.createElement('br')
+const secondBreak = document.createElement('br')
+const nameList = document.createElement('dl')
+const defTitle = document.createElement('dt')
+const defContent = document.createElement('dd')
+displayOfPlayers.textContent = ''
 
 
 
-playerSearch.addEventListener('submit', event =>{
+playerSearch.addEventListener('input', event =>{
+    const _userInput = event.target.value;
     let playerInfo = fetch(url).then(response => response.json())
     .then(data => {
         console.log(data)
-    })
-    })
-    
+        console.log(data.Players[5].jersey)
+        let playersFound = data.Players.filter(player =>{
+            return player.displayName.indexOf(_userInput) >= 0;
+        })
+        console.log(playersFound)
+    });  
+    const NameSection = document.createElement('div')
+    const firstbreak = document.createElement('br')
+    const secondBreak = document.createElement('br')
+    const NameList = document.createElement('dl')
+    const defTitle = document.createElement('dt')
+    const defContent = document.createElement('dd')
+    displayOfPlayers.textContent = ''
 
+});
 
