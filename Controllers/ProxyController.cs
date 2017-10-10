38c4b1e2ca8091880416d8d61a92202166d76799
Robby Bourne <robbybourne1@gmail.com>
2017-10-10 11:19:13 -0400

    Trying to do a workaround

---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using fantasyFootball.Models;
+
+namespace fantasyFootball.Controllers
+{
+    public class ProxyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            // 016705457776214443318:swtmrdurlc4 Public URLGet code
+
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your JavaScript code.
+let playerSearch = document.querySelector('.searchPlayerFn')
+let playerForm = document.querySelector('form')
+let input = document.querySelector('input')
+let search = input.value
+let url = "http://anyorigin.com/go?url=https://www.fantasyfootballnerd.com/service/players/json/yftn2uw58qsv/"
+
+let playerInfo = fetch(url).then(response => response.json()).then(data => data.results)
+
+
+playerSearch.addEventListener('input', event =>{
+    console.log(playerInfo)
+})
+
+
