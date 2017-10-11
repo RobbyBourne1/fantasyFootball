2fda52505efaa3482214719f6e32c9e79c590607
Robby Bourne <robbybourne1@gmail.com>
2017-10-10 21:25:26 -0400

    Working on some stuff

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,5 +9,6 @@ namespace fantasyFootball.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,57 +6,56 @@ let search = input.value
 let url = "/proxy"
 let displayOfPlayers = document.querySelector('.playerInfo')
 
-playerSearch.addEventListener('input', event =>{
+playerSearch.addEventListener('input', event => {
     const _userInput = event.target.value
 
     let playerInfo = fetch(url).then(response => response.json())
-    .then(data => {
-        console.log(data)
-        console.log(data.Players[5].jersey)
-        let playersFound = data.Players.filter(player =>{
-            return player.displayName.indexOf(_userInput) >= 0;
-        })
-        console.log(playersFound)
-        playersFound.forEach(function(playerData, index) {
-            const nameSection = document.createElement('div')
-            const infoSection = document.createElement('div')
-            const firstbreak = document.createElement('br')
-            const secondBreak = document.createElement('br')
-            const thirdBreak = document.createElement('br')
-            const fourthBreak = document.createElement('br')
-            const nameList = document.createElement('dl')
-            const defPN = document.createElement('dt')
-            const defPP = document.createElement('dt')
-            const defPT = document.createElement('dt')
-            const PNContent = document.createElement('dd')
-            const PPContent = document.createElement('dd')
-            const PTContent = document.createElement('dd')
-            displayOfPlayers.textContent = ''
+        .then(data => {
+            console.log(data)
+            console.log(data.Players[5].jersey)
+            let playersFound = data.Players.filter(player => {
+                return player.displayName.indexOf(_userInput) >= 0;
+            })
+            console.log(playersFound)
+            playersFound.forEach(function (playerData, index) {
+                    const nameSection = document.createElement('div')
+                    const infoSection = document.createElement('div')
+                    const firstbreak = document.createElement('br')
+                    const secondBreak = document.createElement('br')
+                    const thirdBreak = document.createElement('br')
+                    const fourthBreak = document.createElement('br')
+                    const nameList = document.createElement('dl')
+                    const defPN = document.createElement('dt')
+                    const defPP = document.createElement('dt')
+                    const defPT = document.createElement('dt')
+                    const PNContent = document.createElement('dd')
+                    const PPContent = document.createElement('dd')
+                    const PTContent = document.createElement('dd')
+                    displayOfPlayers.textContent = ''
 
-            defPN.textContent = "Player Name"
-            PNContent.textContent = playerData.displayName
-            defPP.textContent = "Player Position"
-            PPContent.textContent = playerData.position
-            defPT.textContent = "Player Team"
-            PTContent.textContent = playerData.team
+                    defPN.textContent = "Player Name"
+                    PNContent.textContent = playerData.displayName
+                    defPP.textContent = "Player Position"
+                    PPContent.textContent = playerData.position
+                    defPT.textContent = "Player Team"
+                    PTContent.textContent = playerData.team
 
-            displayOfPlayers.appendChild(nameSection)
-            nameSection.setAttribute('class', 'playerInfo row')
-            nameSection.appendChild(infoSection)
-            nameSection.appendChild(firstbreak)
-            nameSection.appendChild(secondBreak)
-            nameSection.appendChild(nameList)
-            nameList.setAttribute('class', 'dl-horizontal col-sm-12')
-            nameList.appendChild(defPN)
-            nameList.appendChild(PNContent)
-            nameList.appendChild(thirdBreak)
-            nameList.appendChild(defPP)
-            nameList.appendChild(PPContent)
-            nameList.appendChild(fourthBreak)
-            nameList.appendChild(defPT)
-            nameList.appendChild(PTContent)
-            
-        }, this);
-    });  
+                    displayOfPlayers.appendChild(nameSection)
+                    nameSection.setAttribute('class', 'playerInfo row')
+                    nameSection.appendChild(infoSection)
+                    nameSection.appendChild(firstbreak)
+                    nameSection.appendChild(secondBreak)
+                    nameSection.appendChild(nameList)
+                    nameList.setAttribute('class', 'dl-horizontal col-sm-12')
+                    nameList.appendChild(defPN)
+                    nameList.appendChild(PNContent)
+                    nameList.appendChild(thirdBreak)
+                    nameList.appendChild(defPP)
+                    nameList.appendChild(PPContent)
+                    nameList.appendChild(fourthBreak)
+                    nameList.appendChild(defPT)
+                    nameList.appendChild(PTContent)
+            }, this);
+        });
 });
 
