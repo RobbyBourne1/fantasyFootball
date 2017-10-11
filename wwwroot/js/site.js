93b2c648e6b1093c948265e54337fe822c2ba05b
Robby Bourne <robbybourne1@gmail.com>
2017-10-11 09:54:58 -0400

    Cleaned Up JS Code and Only can search 10 at a time

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,7 +4,6 @@ let playerForm = document.querySelector('form')
 let input = document.querySelector('input')
 let search = input.value
 let url = "/proxy"
-let displayOfPlayers = document.querySelector('.playerInfo')
 
 playerSearch.addEventListener('input', event => {
     const _userInput = event.target.value
@@ -16,45 +15,22 @@ playerSearch.addEventListener('input', event => {
             let playersFound = data.Players.filter(player => {
                 return player.displayName.indexOf(_userInput) >= 0;
             })
-            console.log(playersFound)
-            playersFound.forEach(function (playerData, index) {
-                    const nameSection = document.createElement('div')
-                    const infoSection = document.createElement('div')
-                    const firstbreak = document.createElement('br')
-                    const secondBreak = document.createElement('br')
-                    const thirdBreak = document.createElement('br')
-                    const fourthBreak = document.createElement('br')
-                    const nameList = document.createElement('dl')
-                    const defPN = document.createElement('dt')
-                    const defPP = document.createElement('dt')
-                    const defPT = document.createElement('dt')
-                    const PNContent = document.createElement('dd')
-                    const PPContent = document.createElement('dd')
-                    const PTContent = document.createElement('dd')
-                    displayOfPlayers.textContent = ''
+            let displayOfPlayers = document.querySelector('.playerInfo')
+            displayOfPlayers.innerHTML = ''
+            playersFound.slice(0, 10).forEach(function (playerData, index) {
 
-                    defPN.textContent = "Player Name"
-                    PNContent.textContent = playerData.displayName
-                    defPP.textContent = "Player Position"
-                    PPContent.textContent = playerData.position
-                    defPT.textContent = "Player Team"
-                    PTContent.textContent = playerData.team
-
-                    displayOfPlayers.appendChild(nameSection)
-                    nameSection.setAttribute('class', 'playerInfo row')
-                    nameSection.appendChild(infoSection)
-                    nameSection.appendChild(firstbreak)
-                    nameSection.appendChild(secondBreak)
-                    nameSection.appendChild(nameList)
-                    nameList.setAttribute('class', 'dl-horizontal col-sm-12')
-                    nameList.appendChild(defPN)
-                    nameList.appendChild(PNContent)
-                    nameList.appendChild(thirdBreak)
-                    nameList.appendChild(defPP)
-                    nameList.appendChild(PPContent)
-                    nameList.appendChild(fourthBreak)
-                    nameList.appendChild(defPT)
-                    nameList.appendChild(PTContent)
+                    displayOfPlayers.innerHTML += `
+                      <div class="playerInfo row">
+                        <dl class="dl-horizontal col-sm-12">
+                            <dt>Player Name</dt>
+                            <dd>${playerData.displayName}</dd>
+                            <dt>Player Position</dt>
+                            <dd>${playerData.position}</dd>
+                            <dt>Player Team</dt>
+                            <dd>${playerData.team}</dd>
+                        </dl>
+                      </div>
+                    `;
             }, this);
         });
 });
