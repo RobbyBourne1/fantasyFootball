4a5f716f91730a1ff5377e5598b8b9f4faaf5f98
Robby Bourne <robbybourne1@gmail.com>
2017-10-11 22:39:13 -0400

    The page is referencing the wrong JS here

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Write your JavaScript code.
+﻿// Input Players JS
 let playerSearch = document.querySelector('.searchPlayerName')
 let playerForm = document.querySelector('form')
 let input = document.querySelector('input')
@@ -62,3 +62,33 @@ playerSearch.addEventListener('input', event => {
         });
 });
 
+// All Players Page JS
+
+let allPlayers = document.querySelector('.allPlayers')
+let allPlayerBody = document.querySelector('.allPlayerBody')
+let allPlayerSearch = document.querySelector('.searchAllPlayerName')
+
+allPlayerSearch.addEventListener('input', event => {
+    const _userInput = event.target.value
+
+let allPlayerInfo = fetch(url).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let playersFound = data.Players.filter(player => {
+            return player.displayName.indexOf(_userInput) >= 0;
+        })
+            displayOfPlayers.innerHTML = ''
+            playersFound.slice(0, 15).forEach(function (playerData, index) {
+                
+                allPlayerBody.innerHTML += `
+                <tr>
+                    <td><a asp-area="" asp-controller="PlayerInfo" asp-action="Index">${playerData.displayName}</a></td>
+                    <td>${playerData.position}</td>
+                    <td>${playerData.team}</td>
+                    <td>${playerData.active}</td>
+                    <td>${playerData.college}</td>
+                </tr>
+              `;
+            })
+    })
+})
