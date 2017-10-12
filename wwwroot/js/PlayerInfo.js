5a2fa7b4aa2eb9331e4d56d27ddb836a49f650b4
Robby Bourne <robbybourne1@gmail.com>
2017-10-12 13:09:35 -0400

    Javascript queries from URL and Grabs player info from JSON

--- wwwroot/js/PlayerInfo.js
+++ wwwroot/js/PlayerInfo.js
@@ -1,10 +1,33 @@
 
-let url = "/proxy"
+let url = '/proxy'
+
+function getPlayerId() {
+    let searchstring = window.location.pathname
+    let parts = searchstring.split('/')
+    return parts[parts.length - 1];
+}
 
 fetch(url).then(response => response.json())
-.then(data => {
-    console.log(data)
-    let playersFound = data.Players.filter(player => {
-        return player.displayName.indexOf(_userInput) >= 0;
+    .then(data => {
+        console.log(data)
+        let playersFound = data.Players.filter(player => {
+            return player.playerId === getPlayerId();
+        })
+        console.log(playersFound)
+        let indvPlayerNT = document.querySelector('.indvPlayerNT')
+        indvPlayerNT.innerHTML = `
+                <dt>Player Name</dt>
+                <dd>${playersFound[0].displayName}</dd>
+                <br>
+                <dt>Player Team</dt>
+                <dd>${playersFound[0].team}</dd>
+            `
+        let indvPlayerPos = document.querySelector('.indvPlayerPos')
+        indvPlayerPos.innerHTML = `
+                <dt>Player Position</dt>
+                <dd>${playersFound[0].position}</dd>
+                <br>
+                <dt>Player Status</dt>
+                <dd>${playersFound[0].active}</dd>
+            `
     })
-})
