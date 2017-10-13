740d9fdf1d9016156cee0f78b5cbd1373f8da5ea
Robby Bourne <robbybourne1@gmail.com>
2017-10-13 01:25:44 -0400

    Kicker is having an error

--- Controllers/PlayerInfoController.cs
+++ Controllers/PlayerInfoController.cs
@@ -17,7 +17,21 @@ namespace fantasyFootball.Controllers
             var name = $"{finitial}.{lname}";
             position = position.ToLower().ToString();
             var pos = position;
-            var url = $"http://www.footballoutsiders.com/stats/{pos}";
+            var url = $"http://www.footballoutsiders.com/stats/";
+
+            if (pos == "k")
+            {
+                url = "http://www.footballoutsiders.com/stats/teamst";
+
+            }else if (pos == "def")
+            {
+                url = "http://www.footballoutsiders.com/stats/teamdef";
+
+            }else
+            {
+                url = $"http://www.footballoutsiders.com/stats/{pos}";
+            }
+
             var web = new HtmlWeb();
             var doc = web.Load(url);
             var node = doc.DocumentNode.SelectSingleNode("//table");
@@ -46,17 +60,6 @@ namespace fantasyFootball.Controllers
                                 insertViewModel.INTs = nNode.ChildNodes.ElementAt(35).InnerText;
                             
                             Console.WriteLine("team is:" + nNode.ChildNodes.ElementAt(3).InnerText);
-                            Console.WriteLine("DYAR is:" + nNode.ChildNodes.ElementAt(5).InnerText);
-                            Console.WriteLine("DYARRank is:" + nNode.ChildNodes.ElementAt(7).InnerText);
-                            Console.WriteLine("DVOA is:" + nNode.ChildNodes.ElementAt(13).InnerText);
-                            Console.WriteLine("DVOARank is:" + nNode.ChildNodes.ElementAt(15).InnerText);
-                            Console.WriteLine("QBR is:" + nNode.ChildNodes.ElementAt(19).InnerText);
-                            Console.WriteLine("QBRRank is:" + nNode.ChildNodes.ElementAt(21).InnerText);
-                            Console.WriteLine("Yards is:" + nNode.ChildNodes.ElementAt(25).InnerText);
-                            Console.WriteLine("EYards is:" + nNode.ChildNodes.ElementAt(27).InnerText);
-                            Console.WriteLine("TD is:" + nNode.ChildNodes.ElementAt(29).InnerText);
-                            Console.WriteLine("FumblesLost is:" + nNode.ChildNodes.ElementAt(33).InnerText);
-                            Console.WriteLine("INT is:" + nNode.ChildNodes.ElementAt(35).InnerText);
 
                             for (var i = 0; i < nNode.ChildNodes.Count(); i++)
                             {
@@ -118,6 +121,39 @@ namespace fantasyFootball.Controllers
                             insertViewModel.FumblesLost = nNode.ChildNodes.ElementAt(29).InnerText;
                             insertViewModel.CatchRate = nNode.ChildNodes.ElementAt(27).InnerText;
                             insertViewModel.Passes = nNode.ChildNodes.ElementAt(19).InnerText;
+
+                            Console.WriteLine("team is:" + nNode.ChildNodes.ElementAt(3).InnerText);
+
+                            for (var i = 0; i < nNode.ChildNodes.Count(); i++)
+                            {
+                                Console.WriteLine($"{i}:{nNode.ChildNodes[i]}:{nNode.ChildNodes[i].InnerHtml}");
+                            }
+                            return View(insertViewModel);
+                        }
+                    }
+                }
+            }
+            if (position == "k")
+            {
+                foreach (var nNode in node.Descendants("tr"))
+                {
+                    if (nNode.NodeType == HtmlNodeType.Element)
+                    {
+                        var _nameNode = nNode.ChildNodes.FirstOrDefault(n => n.InnerText == name);
+                        if (_nameNode != null)
+                        {
+                            var insertViewModel = new FootBallOViewModel();
+                            insertViewModel.Position = position;
+                            // insertViewModel.DYAR = nNode.ChildNodes.ElementAt(5).InnerText;
+                            // // insertViewModel.DYARRank = nNode.ChildNodes.ElementAt(7).InnerText;
+                            // // insertViewModel.DVOA = nNode.ChildNodes.ElementAt(13).InnerText;
+                            // // insertViewModel.DVOARank = nNode.ChildNodes.ElementAt(15).InnerText;
+                            // // insertViewModel.Yards = nNode.ChildNodes.ElementAt(21).InnerText;
+                            // // insertViewModel.EYards = nNode.ChildNodes.ElementAt(23).InnerText;
+                            // // insertViewModel.TDs = nNode.ChildNodes.ElementAt(25).InnerText;
+                            // // insertViewModel.FumblesLost = nNode.ChildNodes.ElementAt(29).InnerText;
+                            // // insertViewModel.CatchRate = nNode.ChildNodes.ElementAt(27).InnerText;
+                            // // insertViewModel.Passes = nNode.ChildNodes.ElementAt(19).InnerText;
 
                             Console.WriteLine("team is:" + nNode.ChildNodes.ElementAt(3).InnerText);
 
