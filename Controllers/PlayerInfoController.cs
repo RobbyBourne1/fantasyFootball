413d9387839f777eee62400988bbeba7f5455f21
Robby Bourne <robbybourne1@gmail.com>
2017-10-14 11:36:42 -0400

    Pulling Data From Fantasy Pros in regards to specific player

--- Controllers/PlayerInfoController.cs
+++ Controllers/PlayerInfoController.cs
@@ -31,27 +31,11 @@ namespace fantasyFootball.Controllers
                 url = $"http://www.footballoutsiders.com/stats/{pos}";
             }
 
-            var FPName = $"{finitial.ToString()} {lname.ToString()} {team.ToString()}";
+            var FPName = $"{finitial.ToString()}{lname.ToString()}{team.ToString()}";
             var FPurl = $"https://www.fantasypros.com/nfl/projections/{pos}.php";
             var FPweb = new HtmlWeb();
             var FPdoc = FPweb.Load(FPurl);
             var FPnode = FPdoc.DocumentNode.SelectSingleNode("//table/tbody");
-            
-            // Console.WriteLine(FOnode.OuterHtml);
-
-            foreach (var nNode in FPnode.Descendants("tr"))
-            {
-            if (nNode.NodeType == HtmlNodeType.Element)
-                {
-                    
-                        var findName = nNode.ChildNodes.Where(w => w.InnerText == FPName).Select(s => s.InnerText).FirstOrDefault();
-                        if (findName != null)
-                        {
-                          Console.WriteLine(nNode.ChildNodes[0].InnerText);  
-                        }
-                    // I want it to get the inner text of a and match to FP Name. 
-                }
-            }
 
             // Console.WriteLine(url);
             // Console.WriteLine(position);
@@ -62,6 +46,20 @@ namespace fantasyFootball.Controllers
 
             if (position == "qb")
             {   
+                foreach (var nNode in FPnode.Descendants("tr"))
+                {
+                if (nNode.NodeType == HtmlNodeType.Element)
+                    {
+                        var _nameNode = nNode.ChildNodes.FirstOrDefault(n => n.InnerText.Replace(" ", "") == FPName);
+                            if (_nameNode != null)
+                            {
+                                for (var i = 0; i < nNode.ChildNodes.Count(); i++)
+                                {
+                                Console.WriteLine($"{i}:{nNode.ChildNodes[i]}:{nNode.ChildNodes[i].InnerText}");
+                                }
+                            }
+                    }
+                }
                 finitial = finitial.First().ToString();
                 var name = $"{finitial}.{lname}";
                 foreach (var nNode in node.Descendants("tr"))
@@ -87,14 +85,14 @@ namespace fantasyFootball.Controllers
                             
                             Console.WriteLine("team is:" + nNode.ChildNodes.ElementAt(3).InnerText);
 
-                            // for (var i = 0; i < nNode.ChildNodes.Count(); i++)
-                            // {
-                            //     Console.WriteLine($"{i}:{nNode.ChildNodes[i]}:{nNode.ChildNodes[i].InnerHtml}");
-                            // }
+                            for (var i = 0; i < nNode.ChildNodes.Count(); i++)
+                            {
+                                Console.WriteLine($"{i}:{nNode.ChildNodes[i]}:{nNode.ChildNodes[i].InnerHtml}");
+                            }
                             return View(insertViewModel);
                         }
                     }
-                }                
+                }
             }
             if (position == "rb")
             {
---
+++ ViewModels/FantasyProsViewModel.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace fantasyFootball.Models
+{
+    public class FantasyProsViewModel
+    {
+        public string PassAtt { get; set; }
+        public string PassCMP { get; set; }
+        public string PassTDs { get; set; }
+        public string PassINTs { get; set; }
+        public string RushAtt { get; set; }
+        public string RushYards { get; set; }
+        public string RushTDs { get; set; }
+        public string Receptions { get; set; }
+        public string ReceivingYards { get; set; }
+        public string ReceivingTDs { get; set; }
+        public string FGs { get; set; }
+        public string FGAttempted { get; set; }
+        public string XPTs { get; set; }
+        public string Sacks { get; set; }
+        public string INTs { get; set; }
+        public string FRs { get; set; }
+        public string MyProperty { get; set; }
+        public string DSTTDs { get; set; }
+        public string PointsAllowed { get; set; }
+        public string Safetys { get; set; }
+        public string FumblesLost { get; set; }
+        public string FantasyPoints { get; set; }
+    }
+}
